edf1ab0497cad770757b3767e9b23adc085e84f5
georgisenze <georginobouko3@gmail.com>
2023-04-12 23:27:30 +0200

    update pages.yml

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,6 +2,6 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-    // site: 'https://georgisenze.github.io',
+    site: 'https://georgisenze.github.io',
     base: '/portfolio',
   })
